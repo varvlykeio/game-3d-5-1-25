5bd05dacaca6917b54e640a9570fbdde21f7200f
fk207 <fotiskollias@gmail.com>
2025-02-16 00:22:35 +0200

    Created  SendValue function to save values both to file and to variable holder (GameEvents)

--- Assets/Scripts/Menu Scripts/StartMenu.cs
+++ Assets/Scripts/Menu Scripts/StartMenu.cs
@@ -5,6 +5,7 @@ using GameEv;
 using UnityEditor;
 using TextReader;
 using Unity.VisualScripting;
+using UnityEngine.UIElements;
 public class StartMenu : MonoBehaviour
 {
     public GameObject MenuCanvas;
@@ -17,7 +18,9 @@ public class StartMenu : MonoBehaviour
     public GameEvents events;
     public TXTReader Text;
     public int TTime;
-    public int TimeImpact;
+    public int TimeImpact;    
+    int p;
+    int p2;
 
     public void StartGame(){
         events.isPaused = false;
@@ -34,10 +37,9 @@ public class StartMenu : MonoBehaviour
         
         if (TimeImpact>=0 && TimeImpact<=100){
             events.timegravity = TimeImpact; 
-            events.rtime = TTime;
-            Text.VarList[0] = TTime.ToString();
             Debug.Log(Text.VarList[0]);
-            events.TotalTime = TTime;
+
+            SendValue("TotalTime","rtime", TTime);
             StartCoroutine(SaveIcon());
             SwitchToMainMenu();
         }
@@ -46,18 +48,17 @@ public class StartMenu : MonoBehaviour
         }
         Debug.Log("Time: " + events.rtime + " Time Impact: " + events.timegravity);
     }
-    public void ReadTime(string s){
+
+  public void ReadTime(string s){
         if (!string.IsNullOrEmpty(s)){
             TTime = int.Parse(s);
         }
-        //Debug.Log("Time: " + TTime);
     }
-    
+
     public void ReadTimeImpact( string s){
         if (!string.IsNullOrEmpty(s)){
             TimeImpact = int.Parse(s);
         }
-        //Debug.Log("Time Impact: " + TimeImpact);
     }
 
 
@@ -112,4 +113,105 @@ public class StartMenu : MonoBehaviour
         TeacherSettingButtons.SetActive(false);
     }
     
+    
+    ///<summary>
+    /// Function that send values from the setting panel to the save file and the variable holder(GameEvents).
+    /// >Add a second string between the first string and the Value for it to reference two variables in the GameEvents
+    /// >Add a second string after the value to reference two variables in save file
+    /// >Add a second string after the first string and an int after the value to reference two variables in both locations
+    /// </summary>
+    public void SendValue(string variableName, object variable){
+        p = Text.ComList.IndexOf(variableName);
+        if (variable is string strValue){
+            Text.ComList[p] = strValue;
+            typeof(GameEvents).GetField(variableName).SetValue(events, strValue);
+        }
+        else if (variable is int intValue){
+            Text.ComList[p] = intValue.ToString();
+            typeof(GameEvents).GetField(variableName).SetValue(events, intValue);
+        }
+        else if (variable is float floatValue){
+            Text.ComList[p] = floatValue.ToString();
+            typeof(GameEvents).GetField(variableName).SetValue(events, floatValue);
+        }
+        else if (variable is bool boolValue){
+            Text.ComList[p] = boolValue.ToString();
+            typeof(GameEvents).GetField(variableName).SetValue(events, boolValue);
+        }      
+    }
+    public void SendValue(string variableName,string variableName2, object variable){
+        p = Text.ComList.IndexOf(variableName);
+        if (variable is string strValue){
+            Text.ComList[p] = strValue;
+            typeof(GameEvents).GetField(variableName).SetValue(events, strValue);
+            typeof(GameEvents).GetField(variableName2).SetValue(events, strValue);
+        }
+        else if (variable is int intValue){
+            Text.ComList[p] = intValue.ToString();
+            typeof(GameEvents).GetField(variableName).SetValue(events, intValue);
+            typeof(GameEvents).GetField(variableName2).SetValue(events, intValue);
+        }
+        else if (variable is float floatValue){
+            Text.ComList[p] = floatValue.ToString();
+            typeof(GameEvents).GetField(variableName).SetValue(events, floatValue);
+            typeof(GameEvents).GetField(variableName2).SetValue(events, floatValue);
+        }
+        else if (variable is bool boolValue){
+            Text.ComList[p] = boolValue.ToString();
+            typeof(GameEvents).GetField(variableName).SetValue(events, boolValue);
+            typeof(GameEvents).GetField(variableName2).SetValue(events, boolValue);
+        }      
+    }
+     public void SendValue(string variableName, object variable,string variableName2){
+        p = Text.ComList.IndexOf(variableName);
+        p2 = Text.ComList.IndexOf(variableName2);   
+        if (variable is string strValue){
+            Text.ComList[p] = strValue;
+            Text.ComList[p2] = strValue;
+            typeof(GameEvents).GetField(variableName).SetValue(events, strValue);
+        }
+        else if (variable is int intValue){
+            Text.ComList[p] = intValue.ToString();
+            Text.ComList[p2] = intValue.ToString();
+            typeof(GameEvents).GetField(variableName).SetValue(events, intValue);
+        }
+        else if (variable is float floatValue){
+            Text.ComList[p] = floatValue.ToString();
+            Text.ComList[p2] = floatValue.ToString();
+            typeof(GameEvents).GetField(variableName).SetValue(events, floatValue);
+        }
+        else if (variable is bool boolValue){
+            Text.ComList[p] = boolValue.ToString();
+            Text.ComList[p2] = boolValue.ToString();
+            typeof(GameEvents).GetField(variableName).SetValue(events, boolValue);
+        }      
+    }
+     public void SendValue(string variableName,string variableName2, object variable,int ch){
+        p = Text.ComList.IndexOf(variableName);
+        p2 = Text.ComList.IndexOf(variableName2);
+        if (variable is string strValue){
+            Text.ComList[p] = strValue;
+            Text.ComList[p2] = strValue;
+            typeof(GameEvents).GetField(variableName).SetValue(events, strValue);
+            typeof(GameEvents).GetField(variableName2).SetValue(events, strValue);
+        }
+        else if (variable is int intValue){
+            Text.ComList[p] = intValue.ToString();
+            Text.ComList[p2] = intValue.ToString();
+            typeof(GameEvents).GetField(variableName).SetValue(events, intValue);
+            typeof(GameEvents).GetField(variableName2).SetValue(events, intValue);
+        }
+        else if (variable is float floatValue){
+            Text.ComList[p] = floatValue.ToString();
+            Text.ComList[p2] = floatValue.ToString();
+            typeof(GameEvents).GetField(variableName).SetValue(events, floatValue);
+            typeof(GameEvents).GetField(variableName2).SetValue(events, floatValue);
+        }
+        else if (variable is bool boolValue){
+            Text.ComList[p] = boolValue.ToString(); 
+            Text.ComList[p2] = boolValue.ToString();
+            typeof(GameEvents).GetField(variableName).SetValue(events, boolValue);
+            typeof(GameEvents).GetField(variableName2).SetValue(events, boolValue);
+        }      
+    }
 }
